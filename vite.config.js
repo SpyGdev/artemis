5c6274f5c570bd9083af26416ab7b5d4d2f1c3ec
Spy_G <88950166+SpyGdev@users.noreply.github.com>
2025-12-19 23:19:49 +0100

    “Base”

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                socials: resolve(__dirname, 'socials.html'),
+                wallpapers: resolve(__dirname, 'wallpapers.html'),
+                tutorial: resolve(__dirname, 'tutorial.html'),
+            },
+        },
+    },
+})
